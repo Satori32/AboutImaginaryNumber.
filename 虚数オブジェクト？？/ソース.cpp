b7316d586394b6767ec067ea2acea17c60ba3fd5
Satori32 <Huga8989+githubf@gmail.com>
2020-07-07 12:30:49 +0900

    プロジェクト ファイルを追加します。

---
+++ 虚数オブジェクト？？/ソース.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+//Explanation the ‹•”HH
+
+template<class Float=double,class Int = long long int>//[Int] is your Infinity.it is fuse number.‹”B
+class Imaginary {
+public:
+	Imaginary() = delete;
+	Imaginary(const Int& Max):I(Max) {}
+
+	operator Float& () const{
+		return F;
+	}
+
+	operator Float& (){
+		return F;
+	}
+	/**/
+	Float operator =(const Float In) {
+		F = In;
+		return In;
+	}
+	/**/
+	Float operator ()() const{
+		return F * I;
+	}
+
+	bool SetMax(const Int& In) {
+		I = In;
+		return true;
+	}
+	const Int& GetInt() const{
+		return I;
+	}
+	const Float& GetFloat() const{
+		return F;
+	}
+	Float& GetFloat(){
+		return F;
+	}
+protected:
+	Float F=0;
+	Int I=0;
+};
+
+int main() {
+
+	Imaginary<> i = 256;
+
+	i = 10;
+
+	std::cout << i()<<':'<<i.GetInt()<<':'<<i.GetFloat() << std::endl;
+
+	return 0;
+
+}
